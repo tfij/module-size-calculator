1e7965e0141083adcae87cbc4f0934716ff56950
Tomasz Fijalkowski <3066563+tfij@users.noreply.github.com>
2024-02-10 22:58:21 +0100

    Setup checkstyle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    checkstyle
 }
 
 group = "pl.tfij"
@@ -12,8 +13,13 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    checkstyle("pl.tfij:check-tfij-style:1.5.1")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+checkstyle {
+    toolVersion = "10.13.0"
+    sourceSets = listOf(project.sourceSets.main.orNull)
 }
