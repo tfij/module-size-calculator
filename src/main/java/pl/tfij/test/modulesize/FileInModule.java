20de60dd47d77ba00aecfa4e70539c0f34acbd90
Tomasz Fijalkowski <3066563+tfij@users.noreply.github.com>
2024-02-15 21:22:52 +0100

    test recursive sile finding

--- src/main/java/pl/tfij/test/modulesize/FileInModule.java
+++ src/main/java/pl/tfij/test/modulesize/FileInModule.java
@@ -3,4 +3,6 @@ package pl.tfij.test.modulesize;
 import java.util.Optional;
 
 record FileInModule(String file, Optional<Module> module, int lineOfCode) {
+    record Module(String name, String moduleDir) {
+    }
 }
