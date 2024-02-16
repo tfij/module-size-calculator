40a68cd90c85fb0badaead274e7ab7033e07bf2b
Tomasz Fijalkowski <3066563+tfij@users.noreply.github.com>
2024-02-16 22:16:30 +0100

    add test for not existing project dir

--- src/test/java/pl/tfij/test/modulesize/ModuleSizeCalculatorTest.java
+++ src/test/java/pl/tfij/test/modulesize/ModuleSizeCalculatorTest.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 class ModuleSizeCalculatorTest {
 
     @Test
-    @DisplayName("Should analyze all files")
+    @DisplayName("Should analyze all files (recursively)")
     void shouldAnalyzeTestProject() {
         ProjectSummary projectSummary = ModuleSizeCalculator.project("src/test/resources/test-project")
                 .withModule("pl.tfij.commons")
@@ -75,6 +77,20 @@ class ModuleSizeCalculatorTest {
         Assertions.assertEquals(7, projectSummary.modulesSummary().size());
         Assertions.assertEquals(22, projectSummary.numberOfFiles());
         Assertions.assertEquals(1028, projectSummary.linesOfCode());
+    }
+
+    @Test
+    @DisplayName("Should throw ModuleSizeCalculatorException when project dir does not exists")
+    void shouldThrowExceptionIfProjectDirNotExists() {
+        ModuleSizeCalculator.ModuleSizeCalculatorBuilder calculatorForNotExistingProject = ModuleSizeCalculator.project("src/test/resources/not-existing-project")
+                .withModule("pl.tfij.commons");
+
+        ModuleSizeCalculatorException moduleSizeCalculatorException = Assertions.assertThrows(
+                ModuleSizeCalculatorException.class,
+                () -> calculatorForNotExistingProject.analyze());
+
+        String expectedMessageRegex = "Error occur on scanning project in `.*[\\/\\\\]src[\\/\\\\]test[\\/\\\\]resources[\\/\\\\]not-existing-project` directory\\.";
+        Assertions.assertTrue(moduleSizeCalculatorException.getMessage().matches(expectedMessageRegex));
     }
 
 }
